0eb2ec1456ddeac3e9068f5e9a78697cbbd00a53
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-15 09:59:04 +0100

    feat: Make server.js an express server intead of vanila js, use ejs instead of html

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,18 @@
+const express = require("express");
+const app = express();
 
-const socket = io('ws://localhost:8080');
+app.set("view engine", "ejs");
 
-socket.on('message', text => {
-    console.log('this is one data');
-    console.log(text)
-
+app.get("/", (req, res) => {
+  res.render("index");
 });
+
+const io = require("socket.io-client");
+const socket = io("ws://localhost:8080");
+
+socket.on("message", (text) => {
+  console.log("this is one data");
+  console.log(text);
+});
+
+app.listen(3000);
