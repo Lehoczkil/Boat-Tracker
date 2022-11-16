ad0486c15367463db3da625fae9645d43b5adf38
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-16 10:46:58 +0100

    fix: Prevent sending data one more time on a new connection

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 let currentCoordinates = null;
+// port for http for requests for changing protocol
 const httpPort = 3513;
-const port = 3000;
+// port for socket for sending data
+const port = 3100;
 
 // makes the server a client of a mock application sending data
 const io = require("socket.io-client");
@@ -10,23 +12,36 @@ const socket = io("ws://localhost:8080");
 
 // deals with the data coming from the mock application
 socket.on("message", (text) => {
-  console.log("this is one row");
-  console.log(text);
   currentCoordinates = text;
 });
 
 // server for the actual clients, sends the data to them
 const httpServer = require("http").createServer();
 const serverIo = require("socket.io")(httpServer, {
-    cors: { origin: "*" }
+  cors: { origin: "*" },
 });
+
+const frequency = 1000;
+let isSending = false;
+
+async function sendData() {
+  while (true) {
+    serverIo.send(currentCoordinates);
+    // waits for an amount of time to prevent sending data constantly 
+    await new Promise((resolve) => setTimeout(resolve, frequency));
+  }
+}
 
 serverIo.on("connection", () => {
-    console.log('a user connected to express');
-    setInterval(() => {
-      serverIo.send(currentCoordinates)
-    }, 1000)
+  console.log('new connection');
+  // if there is a connection, we are already sending the data, so we shouldn't start sending again on a new connection
+  if (!isSending) {
+    sendData();
+  }
+  isSending = true;
 });
 
-httpServer.listen(httpPort, () => console.log(`listening on http://localhost:${port}`) );
+httpServer.listen(httpPort, () =>
+  console.log(`listening on http://localhost:${port}`)
+);
 app.listen(port);
