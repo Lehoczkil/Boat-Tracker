5f85add87abd1b39fdd4c28f4fb9d83070be2772
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-17 20:07:26 +0100

    fix: Provide data of all boats simultaneously

--- DataProvider/BoatCoordinateProvider.js
+++ DataProvider/BoatCoordinateProvider.js
@@ -1,36 +1,40 @@
-const http = require('http').createServer();
+const http = require("http").createServer();
 const csv = require("csvtojson");
 
 const port = 8080;
 const frequency = 1000;
 
-const io = require('socket.io')(http, {
-    cors: { origin: "*" }
+const io = require("socket.io")(http, {
+  cors: { origin: "*" },
 });
 
-let coordinates1, coordinates2, coordinates3 = null;
+let coordinates1,
+  coordinates2,
+  coordinates3 = null;
 
 async function convertCoordinates() {
-    coordinates1 = await csv().fromFile("./lines/line1.csv")
-    coordinates2 = await csv().fromFile("./lines/line2.csv")
-    coordinates3 = await csv().fromFile("./lines/line3.csv")
+  coordinates1 = await csv().fromFile("./lines/line1.csv");
+  coordinates2 = await csv().fromFile("./lines/line2.csv");
+  coordinates3 = await csv().fromFile("./lines/line3.csv");
 }
 
-async function start_sending_coordinates() {
-    await convertCoordinates();
-    coordinates = [coordinates1, coordinates2, coordinates3];
-    for (coordinate_table of coordinates) {
-        for (coordinate_row of coordinate_table) {
-            io.send(coordinate_row)
-            await new Promise(resolve => setTimeout(resolve, frequency));
-
-        }
-    }
+async function sendCoordinates(data, boat) {
+  for (row of data) {
+    io.emit(boat, row);
+    await new Promise((resolve) => setTimeout(resolve, frequency));
+  }
 }
 
-io.on('connection', async () => {
-    console.log('a user connected');
-    await start_sending_coordinates()
+async function startSendingCoordinates() {
+  await convertCoordinates();
+  sendCoordinates(coordinates1, "boat1");
+  sendCoordinates(coordinates2, "boat2");
+  sendCoordinates(coordinates3, "boat3");
+}
+
+io.on("connection", async () => {
+  console.log("a user connected");
+  await startSendingCoordinates();
 });
 
-http.listen(port, () => console.log(`listening on http://localhost:${port}`) );
+http.listen(port, () => console.log(`listening on http://localhost:${port}`));
