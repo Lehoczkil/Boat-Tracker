0215c3272de3c796c0ebd42263043b8f9cb078ab
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-18 21:13:22 +0100

    feat: Implement visualization of boats

--- react-client/src/components/Home.js
+++ react-client/src/components/Home.js
@@ -4,6 +4,12 @@ import Map from "ol/Map";
 import TileLayer from "ol/layer/Tile";
 import View from "ol/View";
 import XYZ from "ol/source/XYZ";
+import { Vector as VectorLayer } from "ol/layer";
+import VectorSource from "ol/source/Vector";
+import Feature from "ol/Feature";
+import Point from "ol/geom/Point";
+import { Icon, Style } from "ol/style";
+import logo from "./arrow.png";
 
 const Home = ({ socket }) => {
   const [boat1Coordinates, setBoat1Coordinates] = useState("");
@@ -12,42 +18,128 @@ const Home = ({ socket }) => {
 
   const [map, setMap] = useState();
 
+  const [boat1, setBoat1] = useState();
+  const [boat2, setBoat2] = useState();
+  const [boat3, setBoat3] = useState();
+
+  const startingCoordinates = [20.73976894, 48.21548097];
+
   useGeographic();
 
   useEffect(() => {
-    setMap(
-      new Map({
-        target: "map",
-        layers: [
-          new TileLayer({
-            source: new XYZ({
-              url: "https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=14c7611bde4c4982ac1c45ec8b5d80ba",
-            }),
+    setBoat1(
+      new Feature({
+        geometry: new Point(startingCoordinates),
+        style: new Style({
+          image: new Icon({
+            anchor: [0, 0],
+            anchorXUnits: "fraction",
+            anchorYUnits: "pixels",
+            src: logo,
+            scale: 0.1,
           }),
-        ],
-        view: new View({
-          center: [boat1Coordinates.lat, boat1Coordinates.lon],
-          zoom: 16,
+        }),
+      })
+    );
+
+    setBoat2(
+      new Feature({
+        geometry: new Point(startingCoordinates),
+        style: new Style({
+          image: new Icon({
+            anchor: [0, 0],
+            anchorXUnits: "fraction",
+            anchorYUnits: "pixels",
+            src: logo,
+            scale: 0.1,
+          }),
+        }),
+      })
+    );
+
+    setBoat3(
+      new Feature({
+        geometry: new Point(startingCoordinates),
+        style: new Style({
+          image: new Icon({
+            anchor: [0, 0],
+            anchorXUnits: "fraction",
+            anchorYUnits: "pixels",
+            src: logo,
+            scale: 0.1,
+          }),
         }),
       })
     );
   }, []);
 
   useEffect(() => {
-    if (map) {
-      map.getView().setCenter([boat1Coordinates.lon, boat1Coordinates.lat]);
+    if ((boat1, boat2, boat3)) {
+      setMap(
+        new Map({
+          target: "map",
+          layers: [
+            new TileLayer({
+              source: new XYZ({
+                url: "https://{a-c}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=14c7611bde4c4982ac1c45ec8b5d80ba",
+              }),
+            }),
+            new VectorLayer({
+              source: new VectorSource({
+                features: [boat1, boat2, boat3],
+              }),
+            }),
+          ],
+          view: new View({
+            center: startingCoordinates,
+            zoom: 16,
+          }),
+        })
+      );
     }
-  }, [boat1Coordinates, boat2Coordinates, boat3Coordinates]);
+  }, [boat3]);
+
+  useEffect( () => {
+    if ((boat1, boat2, boat3)) {
+      boat1.getGeometry()
+      .setCoordinates(
+        [
+          boat1Coordinates.lon,
+          boat1Coordinates.lat
+        ]
+      );
+
+      boat2.getGeometry()
+      .setCoordinates(
+        [
+          boat2Coordinates.lon,
+          boat2Coordinates.lat
+        ]
+      );
+
+      boat3.getGeometry()
+      .setCoordinates(
+        [
+          boat3Coordinates.lon,
+          boat3Coordinates.lat
+        ]
+      )
+    }
+    if(map) {
+      console.log(boat1.getGeometry().getCoordinates(), 'boat')
+      console.log(map.getLayers())
+    }
+  }, [boat1Coordinates, boat2Coordinates, boat3Coordinates])
 
   useEffect(() => {
     socket.on("boat1", (newCoordinates) => {
       setBoat1Coordinates(newCoordinates);
     });
-    
+
     socket.on("boat2", (newCoordinates) => {
       setBoat2Coordinates(newCoordinates);
     });
-    
+
     socket.on("boat3", (newCoordinates) => {
       setBoat3Coordinates(newCoordinates);
     });
