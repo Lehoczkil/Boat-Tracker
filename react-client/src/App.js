0d271b08229e441d46929a45eed726342f95b1a2
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-20 22:41:14 +0100

    docs: Add comments to app component in react

--- react-client/src/App.js
+++ react-client/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useState } from "react";
 import io from "socket.io-client";
 import Home from "./components/Home";
 
+// Connects to express server
 const socket = io.connect("http://localhost:3513");
 
 function App() {
