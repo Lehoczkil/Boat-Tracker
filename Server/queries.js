1640bd674834fbdcd77586d94b80c9e70af2aedc
lehoczkil <lehoczkilaszlo2002@gmail.com>
2022-11-20 10:22:29 +0100

    fix: Typo in queries

--- Server/queries.js
+++ Server/queries.js
@@ -5,7 +5,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: process.env.USER,
   host: "localhost",
-  database: processe.env.DATABASE,
+  database: process.env.DATABASE,
   password: process.env.PASSWORD,
   port: 5432,
 });
